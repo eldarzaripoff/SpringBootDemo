f5ec9304c176127288bac3c299aa653985e898cd
eldar <eldar.zaripoff@gmail.com>
2023-12-22 19:41:24 +0300

    Commit with internal mistake

---
+++ src/main/java/ru/netology/SpringBootDemoApplication.java
@@ -0,0 +1,12 @@
+package ru.netology;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class SpringBootDemoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootDemoApplication.class, args);
+    }
+}
